c8da10222fc72335104b6fd1f61342555276bdbb
Askaredox <andyecarvajal@gmail.com>
2026-07-16 09:29:32 -0600

    [add]: adding function

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def add(x: int, y: int) -> int:
+    return x + y
+
+
 def main():
     print("Hello, World!")
+    result = add(5, 3)
+    print(f"The sum of 5 and 3 is {result}")
 
 
 if __name__ == "__main__":
